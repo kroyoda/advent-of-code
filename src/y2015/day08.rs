10cde2ac9fc25d35c0f2122fcbbc1d2096a09b05
Anorak <mbecumetailler@gmail.com>
2023-11-22 03:36:55 +0100

    Solved day 8

--- src/y2015/day08.rs
+++ src/y2015/day08.rs
@@ -1,47 +1,72 @@
 #[aoc(day8, part1)]
 pub fn solve_part1(input: &str) -> usize {
-    let mut memory = 0;
-    let mut chars = input.chars();
-    while let Some(c) = chars.next() {
-        if c == '"' {
-            continue;
-        }
-        if c == '\\' {
-            match chars.next() {
-                Some('x') => {
-                    chars.next();
-                    chars.next();
+    input
+        .lines()
+        .map(|line| {
+            let mem_len = line.as_bytes().len();
+
+            let mut char_len = 0;
+            let mut string = &line.as_bytes()[1..mem_len - 1];
+            while !string.is_empty() {
+                let (ch, rest) = string.split_first().expect("empty string");
+                if let b'\\' = ch {
+                    match rest.split_first().expect("invalid string") {
+                        (b'"' | b'\\', _) => {
+                            string = &rest[1..];
+                        }
+                        (b'x', _) => {
+                            string = &rest[3..];
+                        }
+                        _ => unreachable!("unexpected string"),
+                    }
+                } else {
+                    string = rest;
                 }
-                Some(_) => {}
-                None => panic!("Invalid input"),
+                char_len += 1;
             }
-        }
-        memory += 1;
-    }
-    input.len() - memory
+            (mem_len, char_len)
+        })
+        .map(|(m, c)| m - c)
+        .sum()
 }
 
 #[aoc(day8, part2)]
 pub fn solve_part2(input: &str) -> usize {
-    let mut encoded = 0;
+    input
+        .lines()
+        .map(|line| {
+            let mem_len = line.as_bytes().len();
+            let enc_len =
+                mem_len +
+                2 +
+                line
+                    .as_bytes()
+                    .iter()
+                    .filter(|&&b| (b == b'\\' || b == b'"'))
+                    .count();
 
-    encoded - input.len()
+            (mem_len, enc_len)
+        })
+        .map(|(m, e)| e - m)
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const TEST_INPUT: &str = r#"""
+"abc"
+"aaa\"aaa"
+"\x27""#;
+
     #[test]
     fn test_part1() {
-        let lines = [r#""""#, r#""abc""#, r#""aaa\"aaa""#, r#""\x27""#];
-        let literals = [2, 5, 10, 6];
+        assert_eq!(solve_part1(TEST_INPUT), 12);
+    }
 
-        for (i, line) in lines.iter().enumerate() {
-            assert_eq!(line.len(), literals[i]);
-        }
-
-        assert_eq!(solve_part1(lines.join("\n").as_str()), 12);
-        assert_eq!(solve_part2(lines.join("\n").as_str()), 19);
+    #[test]
+    fn test_part2() {
+        assert_eq!(solve_part2(TEST_INPUT), 19);
     }
 }
