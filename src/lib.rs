811876357b60024c9121419c082e54b7fbc5124f
Anorak <mbecumetailler@gmail.com>
2023-11-23 06:30:09 +0100

    Solved day 13

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 extern crate serde_json;
+extern crate itertools;
 extern crate crypto;
 extern crate regex;
 extern crate md5;
