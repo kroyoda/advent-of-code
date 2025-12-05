98ae1b07e86ac57881925b8040cfd5332fe8c258
Maxime Bécu-Métailler <mbecumetailler@onestock-retail.com>
2025-12-05 07:35:08 +0100

    fix: main day 1

--- main.go
+++ main.go
@@ -19,25 +19,19 @@ func run(args []string, _ io.Reader, stdout io.Writer) error {
 
 	switch args[0] {
 	case "day1":
-		instructions := day1.ParseInput(data.Input1)
-		safe := day1.NewSafe()
-		fmt.Fprintln(stdout, safe.Execute(instructions))
-		return nil
+		fmt.Fprintf(stdout, "Part1: %d\n", day1.Part1(data.Input1))
+		fmt.Fprintf(stdout, "Part2: %d\n", day1.Part2(data.Input1))
 	case "day2":
 		fmt.Fprintf(stdout, "Part1: %d\n", day2.Part1(data.Input2))
 		fmt.Fprintf(stdout, "Part2: %d\n", day2.Part2(data.Input2))
-		return nil
 	case "day3":
 		fmt.Fprintf(stdout, "Part1: %d\n", day3.Part1(data.Input3))
 		fmt.Fprintf(stdout, "Part2: %d\n", day3.Part2(data.Input3))
-		return nil
 	case "day4":
 		fmt.Fprintf(stdout, "Part1: %d\n", day4.Part1(data.Input4))
 		fmt.Fprintf(stdout, "Part2: %d\n", day4.Part2(data.Input4))
-		return nil
-	default:
-		return nil
 	}
+	return nil
 }
 
 func main() {
